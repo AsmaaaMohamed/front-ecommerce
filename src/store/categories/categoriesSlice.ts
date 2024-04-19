de8bef4c8541beb899103fe27ceaf667dec6ef10
Asmaa Mohamed <eng_asmaaa20102000@yahoo.com>
2024-04-19 22:28:11 +0200

    first thunk

---
+++ src/store/categories/categoriesSlice.ts
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState={
+    records:[],
+    loading:'idle',
+    error:null
+};
+const categoriesSlice = createSlice({
+    name: 'categories',
+    initialState,
+    reducers:{}
+});
+export default categoriesSlice;
